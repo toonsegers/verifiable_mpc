371adb1852be7c26cfb1489ba15ae6a1b1beebb4
toonsegers <a.j.m.segers@tue.nl>
2022-02-05 14:03:36 +0100

    add sigmaproof example with secret x. work in progress to mix sigmaproofs with circuit proofs

--- verifiable_mpc/wip_sigmaproof.py
+++ verifiable_mpc/wip_sigmaproof.py
@@ -44,3 +44,31 @@ print((B^int(r))*h^int(s))
 print(a*g^int(c))
 print((B^int(r))*(h^int(s)) == a*(g^int(c)))
 
+# Secure groups
+from mpyc.runtime import mpc
+from mpyc.secgroups import repeat_public_base_public_output as secure_repeat
+mpc.run(mpc.start())
+secgrp = mpc.SecGrp(group)
+sectype = mpc.SecFld(modulus=n)
+r = mpc._random(sectype)
+h = mpc.run(secgrp.repeat_public(g, r))
+x, y = sectype(1), sectype(1)
+# B = secgrp.repeat_public(g,x)*secgrp.repeat_public(h,y)
+B = mpc.run(secgrp.repeat_public([g, h], [x, y]))
+u, v = [mpc._random(sectype) for i in range(2)]
+# a = secgrp.repeat_public(B,u)*secgrp.repeat_public(h,v)
+a = mpc.run(secgrp.repeat_public([B, h], [u, v]))
+
+input_list = [a, B]
+c = gf(pivot.fiat_shamir_hash(input_list, gf.order))
+
+# Prover
+r = mpc.run(mpc.gather(u + c/x))
+s = mpc.run(mpc.gather(v - c*y/x))
+
+# Verifier
+print((B^int(r))*h^int(s))
+print(a*g^int(c))
+print((B^int(r))*(h^int(s)) == a*(g^int(c)))
+
+mpc.run(mpc.shutdown())
